7cf73ddfb8222f2af90c1beece98cf42f8443d8d
Kevin Leung <kevinresol@gmail.com>
2018-09-21 23:38:27 +0800

    Cosmetics

--- tests/Test.hx
+++ tests/Test.hx
@@ -10,18 +10,15 @@ import flash.system.System.exit;
 import Sys.exit;
 #end
 
-class Test extends TestCase
-{
-	static function main()
-	{
+class Test extends TestCase {
+	static function main() {
 		var t = new TestRunner();
 		t.add(new Test());
 		
 		exit(t.run() ? 0 : 500);
 	}
 	
-	function testBasic()
-	{
+	function testBasic() {
 		var num_to_hash = 900719925;
 		var	a = new Hashids('this is my salt');
 		var res = a.encode(num_to_hash);
@@ -29,15 +26,13 @@ class Test extends TestCase
 		assertEquals(num_to_hash, b[0]);
 	}
 	
-	function testWrongDecoding()
-	{
+	function testWrongDecoding() {
 		var a = new Hashids('this is my pepper');
 		var b = a.decode('NkK9');
 		assertEquals(0, b.length);
 	}
 
-	function testOneNumber()
-	{
+	function testOneNumber() {
 		var expected = 'NkK9';
 		var num_to_hash = 12345;
 		var a = new Hashids('this is my salt');
@@ -51,8 +46,7 @@ class Test extends TestCase
 		assertEquals(num_to_hash, res2); // test the abstract cast
 	}
 
-	function testServeralNumbers()
-	{
+	function testServeralNumbers() {
 		var expected = 'aBMswoO2UB3Sj';
 		var num_to_hash = [683, 94108, 123, 5];
 		var a = new Hashids('this is my salt');
@@ -66,8 +60,7 @@ class Test extends TestCase
 			assertEquals(num_to_hash[i], res2[i]);
 	}
 
-	function testSpecifyingCustomHashAlphabet()
-	{
+	function testSpecifyingCustomHashAlphabet() {
 		var expected = 'b332db5';
 		var num_to_hash = 1234567;
 		var a = new Hashids('this is my salt', 0, '0123456789abcdef');
@@ -80,8 +73,7 @@ class Test extends TestCase
 		assertEquals(num_to_hash, res2); // test the abstract cast
 	}
 
-	function testSpecifyingCustomHashLength()
-	{
+	function testSpecifyingCustomHashLength() {
 		var expected = 'gB0NV05e';
 		var num_to_hash = 1;
 		var a = new Hashids('this is my salt', 8);
@@ -95,8 +87,7 @@ class Test extends TestCase
 		assertEquals(num_to_hash, res2); // test the abstract cast
 	}
 
-	function testRandomness()
-	{
+	function testRandomness() {
 		var expected = '1Wc8cwcE', res;
 		var num_to_hash = [5, 5, 5, 5];
 		var a = new Hashids('this is my salt');
@@ -110,8 +101,7 @@ class Test extends TestCase
 			assertEquals(num_to_hash[i], res2[i]);
 	}
 
-	function testRandomnessForIncrementingNumbers()
-	{
+	function testRandomnessForIncrementingNumbers() {
 		var expected = 'kRHnurhptKcjIDTWC3sx';
 		var num_to_hash = [1,2,3,4,5,6,7,8,9,10];
 		var a = new Hashids('this is my salt');
@@ -125,8 +115,7 @@ class Test extends TestCase
 			assertEquals(num_to_hash[i], res2[i]);
 	}
 
-	function testRandomnessForIncrementing()
-	{
+	function testRandomnessForIncrementing() {
 		var a = new Hashids('this is my salt');
 		assertEquals('NV', a.encode(1));
 		assertEquals('6m', a.encode(2));
@@ -135,8 +124,7 @@ class Test extends TestCase
 		assertEquals('rD', a.encode(5));
 	}
 
-	function testAlphabetWithoutO0()
-	{
+	function testAlphabetWithoutO0() {
 		var expected = '9Q7MJ3LVGW';
 		var num_to_hash = 1145;
 		var a = new Hashids('MyCamelCaseSalt', 10, 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789');
